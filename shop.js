9f8d1fb8b9b95bc3c17375ab599a8595560769dc
elias1991xNataly <eliasalfonso1991@gmail.com>
2023-06-18 13:33:06 +0200

    fixing errors

--- shop.js
+++ shop.js
@@ -1,6 +1,5 @@
 
 let number = localStorage.getItem("numberOfItems");
-console.log(number);
 let cart = document.querySelector(".cartsection");
 let paragraph = document.createElement("p");
 cart.appendChild(paragraph);
@@ -21,137 +20,161 @@ cart.style.display = "flex";
 cart.style.flexDirection = "row";
 cart.style.gap = "10px";
 
+const addButtonSum2= document.querySelectorAll(".addButton");
+const images=document.querySelectorAll(".imageSetUp");
+const pricesOfCoffees=document.querySelectorAll(".priceOfCoffee");
+const namesOfCoffees=document.querySelectorAll(".nameOfCoffee");
+
+const cart2=[];
+addButtonSum2.forEach((element,key)=>{
+    element.addEventListener("click", ()=>{
+    const object={imagen:"" ,name:"", price:"",count:0};
+   object.imagen=images[key].src;
+   console.log(namesOfCoffees[key].innerText);
+    object.name=namesOfCoffees[key].innerText;
+    object.price=pricesOfCoffees[key].innerText;
+    object.count+=1;
+    console.log(images);
+    cart2.push(object);
+    console.log(cart2);
+    console.log(cart2.length);
+  })
+});
+
+//Métodos de objetos: Object.assign, Object.keys, Object.values, Object.entries
+//hasOwnProperty
 
 
-var shoppingcart = (function () {
-    let cart2 = [];
 
-    //Constructor
-    function Item(image, name, price, count) {
-        this.image = image;
-        this.name = name;
-        this.price = price;
-        this.count = count;
-    }
+// var shoppingcart = (function () {
+//     let cart2 = [];
 
-    //Save cart
-    function saveCart() {
-        sessionStorage.setItem("shoppingCart", JSON.stringify(cart));
-    }
+//     //Constructor
+//     function Item(image, name, price, count) {
+//         this.image = image;
+//         this.name = name;
+//         this.price = price;
+//         this.count = count;
+//     }
 
-    //Load cart
-    function loadCart() {
-        cart2 = JSON.parse(sessionStorage.getItem("shoppingCart"));
-    }
-    if (sessionStorage.getItem("shoppingCart") != null) {
-        loadCart();
-    }
+//     //Save cart
+//     function saveCart() {
+//         sessionStorage.setItem("shoppingCart", JSON.stringify(cart));
+//     }
 
-    // Public methods and propeties
-    var obj = {};
+//     //Load cart
+//     function loadCart() {
+//         cart2 = JSON.parse(sessionStorage.getItem("shoppingCart"));
+//     }
+//     if (sessionStorage.getItem("shoppingCart") != null) {
+//         loadCart();
+//     }
 
-    // Add to cart
-    obj.addItemToCart = function (image, name, price, count) {
-        for (const item of cart2) {
-            if (cart[item].name === name) {
-                cart[item].count++;
-                saveCart();
-                return;
-            }
-        }
-        var item = new Item(image, name, price, count);
-        cart.push(item);
-        saveCart;
-    }
+//     // Public methods and propeties
+//     var obj = {};
 
-
-    // Set count from item
-    obj.setCountForItem = function (name, count) {
-        for (var i in cart) {
-            if (cart[i].name === name) {
-                cart[i].count = count;
-                break;
-            }
-        }
-    };
-    // Remove item from cart
-    obj.removeItemFromCart = function (name) {
-        for (var item in cart) {
-            if (cart[item].name === name) {
-                cart[item].count--;
-                if (cart[item].count === 0) {
-                    cart.splice(item, 1);
-                }
-                break;
-            }
-        }
-        saveCart();
-    }
-
-    // Remove all items from cart
-    obj.removeItemFromCartAll = function (name) {
-        for (var item in cart) {
-            if (cart[item].name === name) {
-                cart.splice(item, 1);
-                break;
-            }
-        }
-        saveCart();
-    }
+//     // Add to cart
+//     obj.addItemToCart = function (image, name, price, count) {
+//         for (const item of cart2) {
+//             if (cart[item].name === name) {
+//                 cart[item].count++;
+//                 saveCart();
+//                 return;
+//             }
+//         }
+//         var item = new Item(image, name, price, count);
+//         cart.push(item);
+//         saveCart;
+//     }
 
 
-    // Clear cart
-    obj.clearCart = function () {
-        cart = [];
-        saveCart();
-    }
+//     // Set count from item
+//     obj.setCountForItem = function (name, count) {
+//         for (var i in cart) {
+//             if (cart[i].name === name) {
+//                 cart[i].count = count;
+//                 break;
+//             }
+//         }
+//     };
+//     // Remove item from cart
+//     obj.removeItemFromCart = function (name) {
+//         for (var item in cart) {
+//             if (cart[item].name === name) {
+//                 cart[item].count--;
+//                 if (cart[item].count === 0) {
+//                     cart.splice(item, 1);
+//                 }
+//                 break;
+//             }
+//         }
+//         saveCart();
+//     }
 
-    // Count cart 
-    obj.totalCount = function () {
-        var totalCount = 0;
-        for (var item in cart) {
-            totalCount += cart[item].count;
-        }
-        return totalCount;
-    }
-
-    // Total cart
-    obj.totalCart = function () {
-        var totalCart = 0;
-        for (var item in cart) {
-            totalCart += cart[item].price * cart[item].count;
-        }
-        return Number(totalCart.toFixed(2));
-    }
-
-    // List cart
-    obj.listCart = function () {
-        var cartCopy = [];
-        for (i in cart) {
-            item = cart[i];
-            itemCopy = {};
-            for (p in item) {
-                itemCopy[p] = item[p];
-
-            }
-            itemCopy.total = Number(item.price * item.count).toFixed(2);
-            cartCopy.push(itemCopy)
-        }
-        return cartCopy;
-    }
-
-    // cart : Array
-    // Item : Object/Class
-    // addItemToCart : Function
-    // removeItemFromCart : Function
-    // removeItemFromCartAll : Function
-    // clearCart : Function
-    // countCart : Function
-    // totalCart : Function
-    // listCart : Function
-    // saveCart : Function
-    // loadCart : Function
+//     // Remove all items from cart
+//     obj.removeItemFromCartAll = function (name) {
+//         for (var item in cart) {
+//             if (cart[item].name === name) {
+//                 cart.splice(item, 1);
+//                 break;
+//             }
+//         }
+//         saveCart();
+//     }
 
 
-    return obj;
-})();
+//     // Clear cart
+//     obj.clearCart = function () {
+//         cart = [];
+//         saveCart();
+//     }
+
+//     // Count cart 
+//     obj.totalCount = function () {
+//         var totalCount = 0;
+//         for (var item in cart) {
+//             totalCount += cart[item].count;
+//         }
+//         return totalCount;
+//     }
+
+//     // Total cart
+//     obj.totalCart = function () {
+//         var totalCart = 0;
+//         for (var item in cart) {
+//             totalCart += cart[item].price * cart[item].count;
+//         }
+//         return Number(totalCart.toFixed(2));
+//     }
+
+//     // List cart
+//     obj.listCart = function () {
+//         var cartCopy = [];
+//         for (i in cart) {
+//             item = cart[i];
+//             itemCopy = {};
+//             for (p in item) {
+//                 itemCopy[p] = item[p];
+
+//             }
+//             itemCopy.total = Number(item.price * item.count).toFixed(2);
+//             cartCopy.push(itemCopy)
+//         }
+//         return cartCopy;
+//     }
+
+//     // cart : Array
+//     // Item : Object/Class
+//     // addItemToCart : Function
+//     // removeItemFromCart : Function
+//     // removeItemFromCartAll : Function
+//     // clearCart : Function
+//     // countCart : Function
+//     // totalCart : Function
+//     // listCart : Function
+//     // saveCart : Function
+//     // loadCart : Function
+
+
+//     return obj;
+// })();
